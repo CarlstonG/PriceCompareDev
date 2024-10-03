65c82be1d2994c6879363c5cf30127ed01ae78a9
Carlston Grefalde (Office Brands) <cgrefalde@officebrands.com.au>
2024-10-03 08:34:27 +0800

    added auto browser pop upon server open

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     host: '0.0.0.0',  // This allows it to be accessible via your local network
-    port: 3000,        // Specify the port (you can change it as needed)
+    port: 3000,  
+    open: true,      // Specify the port (you can change it as needed)
   },
 });
